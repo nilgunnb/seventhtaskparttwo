c1051fc818b5ece052618331d3675534824eaeb3
nilgunnb <nilgunnb@code.edu.az>
2023-06-25 15:22:46 +0400

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-let menu = document.querySelector("#menu");
-let sidenav = document.querySelector(".sidenav");
-
-menu.onclick = function (){
-   sidenav.style.width = "100%"
+let menu = document.querySelector("#menu")
+menu.onclick = function (e){
+  e.preventDefault();
+  document.querySelector("#mySidenav").style.width = "100%"
+  console.log("salam")
 }
 
 function closeNav(){
-    sidenav.style.width = "0"
+    document.querySelector("#mySidenav").style.width = "0"
 }
